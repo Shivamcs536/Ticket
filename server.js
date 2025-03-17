74b5da4d20f060de84ab330f4bf295bd693759e4
Shivamcs536 <157484542+Shivamcs536@users.noreply.github.com>
2025-03-17 07:12:36 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const app = express();
 const PORT = 3000;
 
-// MySQL Connection
+
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -13,14 +13,14 @@ const db = mysql.createConnection({
 
 db.connect(err => err ? console.error(err) : console.log('MySQL Connected'));
 
-// Middleware
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Routes
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
-// Create a ticket
+
 app.post('/create', (req, res) => {
   const { title, description } = req.body;
   db.query('INSERT INTO tickets (title, description) VALUES (?, ?)', [title, description], err => {
@@ -29,7 +29,7 @@ app.post('/create', (req, res) => {
   });
 });
 
-// View all tickets
+
 app.get('/tickets', (req, res) => {
   db.query('SELECT * FROM tickets', (err, results) => {
     if (err) throw err;
@@ -44,7 +44,7 @@ app.get('/tickets', (req, res) => {
   });
 });
 
-// Edit a ticket
+
 app.get('/edit/:id', (req, res) => {
   const { id } = req.params;
   db.query('SELECT * FROM tickets WHERE id = ?', [id], (err, results) => {
@@ -62,7 +62,7 @@ app.get('/edit/:id', (req, res) => {
   });
 });
 
-// Update a ticket
+
 app.post('/update/:id', (req, res) => {
   const { id } = req.params;
   const { title, description } = req.body;
@@ -72,7 +72,7 @@ app.post('/update/:id', (req, res) => {
   });
 });
 
-// Delete a ticket
+
 app.get('/delete/:id', (req, res) => {
   const { id } = req.params;
   db.query('DELETE FROM tickets WHERE id = ?', [id], err => {
@@ -81,5 +81,5 @@ app.get('/delete/:id', (req, res) => {
   });
 });
 
-// Start Server
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
